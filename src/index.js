706d1a00401f370dfb15f39625f351875df7826e
Muhammed Nabeel <95733408+muhammednabeel123@users.noreply.github.com>
2025-05-06 08:12:37 +0530

    ssl changes

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ const pool = new Pool({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
+  ssl: process.env.PG_SSL === 'true' ? { rejectUnauthorized: false } : false
 });
 
 const amadeus = new Amadeus({
